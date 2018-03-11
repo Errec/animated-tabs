dcb9c49ad0cc6f3f000e355dcbae4ff05fca5775
Raniro Coelho <errecme@gmail.com>
2018-03-10 23:48:46 -0300

    Add _setTabGroupClick function

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 "use strict";
 var initTabs = (function () {
   var tabGroup0  = document.getElementById("tab-group-0");
-  var tabs0      = document.querySelectorAll("#tab-group-0 > li");
-  var tabSlected = tabs0[0];
+  var tabsItems0 = document.querySelectorAll("#tab-group-0 > li");
 
-  tabGroup0.addEventListener('click', function (e) {
-    if (e.target.dataset.tab && (tabSlected.dataset.tab !== e.target.dataset.tab)) {
-      tabSlected.classList.remove('selected');
-      tabSlected = tabs0[parseInt(e.target.dataset.tab)];
-      tabSlected.classList.add('selected');
-    }
-  });
+  _setTabGroupClick(tabGroup0, tabsItems0, tabsItems0[0]);
+
+  function _setTabGroupClick (tabGroup, tabsItems, tabSlected) {
+    tabGroup.addEventListener('click', function (e) {
+      if (e.target.dataset.tab && (tabSlected.dataset.tab !== e.target.dataset.tab)) {
+        tabSlected.classList.remove('selected');
+        tabSlected = tabsItems[parseInt(e.target.dataset.tab)];
+        tabSlected.classList.add('selected');
+      }
+    });
+  }
 })();
