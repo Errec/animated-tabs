31eb4230d5fe52517c691b4f1f34a50d30277b8c
Raniro Coelho <errecme@gmail.com>
2018-03-12 22:16:00 -0300

    Add tab content switch

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
 "use strict";
 var initTabs = (function () {
-  var tabGroup0  = document.getElementById("tab-group-0");
-  var tabsItems0 = document.querySelectorAll("#tab-group-0 > li");
+  var contentItems = document.querySelectorAll('.content__item');
+  var tabGroup0    = document.getElementById("tab-group-0");
+  var tabsItems0   = document.querySelectorAll("#tab-group-0 > li");
 
-  _setTabGroupClick(tabGroup0, tabsItems0, tabsItems0[0]);
+  _setTabGroupClick(tabGroup0, tabsItems0, 0);
 
-  function _setTabGroupClick (tabGroup, tabsItems, tabSlected) {
+  function _setTabGroupClick (tabGroup, tabsItems, tabIndex) {
+    var tabSlected = tabsItems[tabIndex];
+
     tabGroup.addEventListener('click', function (e) {
       if (e.target.dataset.tab && (tabSlected.dataset.tab !== e.target.dataset.tab)) {
+        var currentTabIndex = parseInt(e.target.dataset.tab);
+
+        var oldTabIndex = parseInt(tabSlected.dataset.tab);
+
         tabSlected.classList.remove('selected');
-        tabSlected = tabsItems[parseInt(e.target.dataset.tab)];
+        tabSlected = tabsItems[currentTabIndex];
         tabSlected.classList.add('selected');
+
+        contentItems[oldTabIndex].classList.remove('content--show');
+        contentItems[currentTabIndex].classList.add('content--show');
       }
     });
   }
