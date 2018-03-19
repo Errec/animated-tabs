fb63e53adac7c2d2e64a917054e27d9b6e96147c
Raniro Coelho <errecme@gmail.com>
2018-03-18 22:38:06 -0300

    Add tab2

--- js/main.js
+++ js/main.js
@@ -8,8 +8,12 @@ var initTabs = (function () {
   var tabGroup1    = document.getElementById("tab-group-1");
   var tabsItems1   = document.querySelectorAll("#tab-group-1 > li");
 
+  var tabGroup2    = document.getElementById("tab-group-2");
+  var tabsItems2   = document.querySelectorAll("#tab-group-2 > li");
+
   _setTabGroupClick(tabGroup0, tabsItems0, 0);
   _setTabGroupClick(tabGroup1, tabsItems1, 0);
+  _setTabGroupClick(tabGroup2, tabsItems2, 0);
 
   function _setTabGroupClick (tabGroup, tabsItems, tabIndex) {
     var tabSlected = tabsItems[tabIndex];
