e2f6a8c02ea29646faead2e9aec82b5f31bab735
Raniro Coelho <errecme@gmail.com>
2018-05-10 11:22:19 -0300

    Update content and tab styles

--- js/main.js
+++ js/main.js
@@ -5,15 +5,15 @@ var initTabs = (function () {
   var tabGroup0    = document.getElementById("tab-group-0");
   var tabsItems0   = document.querySelectorAll("#tab-group-0 > li");
 
-  var tabGroup1    = document.getElementById("tab-group-1");
-  var tabsItems1   = document.querySelectorAll("#tab-group-1 > li");
+  // var tabGroup1    = document.getElementById("tab-group-1");
+  // var tabsItems1   = document.querySelectorAll("#tab-group-1 > li");
 
-  var tabGroup2    = document.getElementById("tab-group-2");
-  var tabsItems2   = document.querySelectorAll("#tab-group-2 > li");
+  // var tabGroup2    = document.getElementById("tab-group-2");
+  // var tabsItems2   = document.querySelectorAll("#tab-group-2 > li");
 
   _setTabGroupClick(tabGroup0, tabsItems0, 0);
-  _setTabGroupClick(tabGroup1, tabsItems1, 0);
-  _setTabGroupClick(tabGroup2, tabsItems2, 0);
+  // _setTabGroupClick(tabGroup1, tabsItems1, 0);
+  // _setTabGroupClick(tabGroup2, tabsItems2, 0);
 
   function _setTabGroupClick (tabGroup, tabsItems, tabIndex) {
     var tabSlected = tabsItems[tabIndex];
